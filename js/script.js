549573ea8cd424112decc210ac512776df0c7575
Salvatore Capano <capano_salvatore@libero.it>
2022-12-20 22:04:33 +0100

    Accesso ai dati del form e reset del form

--- js/script.js
+++ js/script.js
@@ -1,13 +1,46 @@
-// Inserire n. km da percorrere
-const distance = parseInt(prompt("Quanti km vuoi percorrere?"));
+const submitBtm = document.getElementById("submitBtn");
+const resetBtn = document.getElementById("resetBtn");
 
-// Inserire eta' passeggero
-const age = parseInt(prompt("Quanti anni hai?"));
+let passenger = document.querySelector("#passenger");
+let distance = document.querySelector("#distance");
+let age = document.querySelector("#age");
 
-console.log(`Dati viaggio:
-            Km da percorrere: ${distance}
-            Eta': ${age}`);
+let passengerValue;
+let distanceValue;
+let ageValue;
 
+submitBtm.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    console.log("Dati form")
+
+    passengerValue = passenger.value;
+    console.log(passengerValue);
+
+    distanceValue = distance.value;
+    console.log(distanceValue);
+
+    ageValue = age.value;
+    console.log(ageValue);
+
+});
+
+resetBtn.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    console.log("Dati resettati")
+
+    passenger.value = "";
+    distance.value = "";
+    age.value = "";
+
+    console.log(passenger.value);
+    console.log(distance.value);
+    console.log(age.value);
+
+})
 
 /* 
    Calcolo prezzo del viaggio:
@@ -17,40 +50,40 @@ console.log(`Dati viaggio:
 */
 
 // Calcola il prezzo senza lo sconto
-const fullPrice = (distance * 0.21);
-console.log(`Il prezzo intero e' ${fullPrice} €.`);
+// const fullPrice = (distance * 0.21);
+// console.log(`Il prezzo intero e' ${fullPrice} €.`);
 
-let discountPerc;
-let disocuntCheck = false;
+// let discountPerc;
+// let disocuntCheck = false;
 
 // Verifica la correttezza dell'eta'
-if ((age > 0) && (age < 130)) {
-    if(age < 18) {
-        discountPerc = 20; // -->  20%
-        console.log(`Sei minorenne. Hai diritto ad uno sconto del ${discountPerc}%.`);
-        disocuntCheck = true;
-    }
-    else if (age >= 65){
-        discountPerc = 40; // -->  40%
-        console.log(`Sei over 65. Hai diritto ad uno sconto del ${discountPerc}%.`);
-        disocuntCheck = true;
-    }
-    else {
-        console.log("Per la tua fascia d'eta' non sono riservati sconti.");
-    }
+// if ((age > 0) && (age < 130)) {
+//     if(age < 18) {
+//         discountPerc = 20;
+//         console.log(`Sei minorenne. Hai diritto ad uno sconto del ${discountPerc}%.`);
+//         disocuntCheck = true;
+//     }
+//     else if (age >= 65){
+//         discountPerc = 40;
+//         console.log(`Sei over 65. Hai diritto ad uno sconto del ${discountPerc}%.`);
+//         disocuntCheck = true;
+//     }
+//     else {
+//         console.log("Per la tua fascia d'eta' non sono riservati sconti.");
+//     }
 
     // Calcola il valore dello sconto (se lo sconto e' applicabile)
-    let discount = 0
-    if (disocuntCheck) {
-        discount = fullPrice * (discountPerc / 100);
-        console.log(`Il tuo sconto equivale a ${discount.toFixed(2)} €.`);
-    }
+    // let discount = 0
+    // if (disocuntCheck) {
+    //     discount = fullPrice * (discountPerc / 100);
+    //     console.log(`Il tuo sconto equivale a ${discount.toFixed(2)} €.`);
+    // }
 
     // Calcola il prezzo finale
-    let price = fullPrice - discount;
-    console.log(`Il totale e' ${price.toFixed(2)} €.`);
+//     let price = fullPrice - discount;
+//     console.log(`Il totale e' ${price.toFixed(2)} €.`);
 
-}
-else {
-    console.log("Inserisci un eta' valida!");
-}
+// }
+// else {
+//     console.log("Inserisci un eta' valida!");
+// }
